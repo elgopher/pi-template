befd81ad41a98611e02c1be8c14060ddf07d850b
Jacek Olszak <jacekolszak@gmail.com>
2022-10-22 21:22:03 +0200

    Create production and dev build (#3)
    Devtools are enabled by default. When binary is built with `prod` tag, then devtools are disabled.

---
+++ main_prod.go
@@ -0,0 +1,17 @@
+//go:build prod
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/elgopher/pi"
+	"github.com/elgopher/pi/ebitengine"
+)
+
+// this function is executed only for production build
+func run() {
+	if err := pi.Run(ebitengine.Backend); err != nil {
+		fmt.Printf("I'm sorry. Pi game cannot be run :( The error is: %s\n", err.Error())
+	}
+}
